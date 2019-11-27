ff5b8c164d414f902402f365bca83d4a90d535d7
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-27 15:04:10 +0000

    Updating getenv

--- function_getenv.c
+++ function_getenv.c
@@ -7,20 +7,13 @@
  */
 char *_getenv(char *name, char **env)
 {
-	char **save = env;
 	char **tokenizado;
 	int i = 0;
 
+	i = 0;
 	while (env[i] != NULL)
 	{
-		save[i] = env[i];
-		i++;
-	}
-	save[i] = NULL;
-	i = 0;
-	while (save[i] != NULL)
-	{
-		tokenizado = words(*(save + i), "=");
+		tokenizado = words(*(env + i), "=");
 			if (_strcmp(tokenizado[0], name) == 0)
 			{
 				break;
